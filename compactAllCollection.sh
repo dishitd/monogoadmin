1236b05dc1a3c07785ce7ded680fb288d8e6c6a2
Dishit <dishid@gmail.com>
2017-06-19 14:46:38 +1000

    Update compactAllCollection.sh

--- compactAllCollection.sh
+++ compactAllCollection.sh
@@ -1,3 +1,5 @@
+//Originally posted on https://gist.github.com/8548102
+
 db.getCollectionNames().forEach(function (collectionName) {
     print('Compacting: ' + collectionName);
     db.runCommand({ compact: collectionName });
